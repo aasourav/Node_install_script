ab910b0a439020e3c2016b450957c3f7cf4c3978
Ahsan Amin <ahsan.amin@headless.ltd>
2023-06-23 11:46:24 +0600

    arainge link code restructured

--- nodeV18Install.sh
+++ nodeV18Install.sh
@@ -1,13 +1,27 @@
 #bin/bash
+downloadLink="https://nodejs.org/dist/v18.16.1/node-v18.16.1-linux-x64.tar.xz"
+tarFile="~/Downloads/node-v18.16.1-linux-x64.tar.xz"
+extractedTarFile="~/Downloads/node-v18.16.1-linux-x64.tar.xz"
+createDir="/usr/local/lib/nodejs"
+moveExtractedFileFrom="~/Downloads/node-v18.16.1-linux-x64"
+moveExtractedFileTo="/usr/local/lib/nodejs"
+
+eval downloadLink="$downloadLink"
+eval tarFile="$tarFile"
+eval extractedTarFile="$extractedTarFile"
+eval createDir="$createDir"
+eval moveExtractedFileFrom="$moveExtractedFileFrom"
+eval moveExtractedFileTo="$moveExtractedFileTo"
+
 cd ~/Downloads &&
-wget https://nodejs.org/dist/v18.16.1/node-v18.16.1-linux-x64.tar.xz &&
-tar -xvf ~/Downloads/node-v18.16.1-linux-x64.tar.xz &&
-sudo mkdir -p /usr/local/lib/nodejs &&
-sudo mv ~/Downloads/node-v18.16.1-linux-x64 /usr/local/lib/nodejs &&
+wget $downloadLink &&
+tar -xvf $tarFile &&
+sudo mkdir -p $createDir &&
+sudo mv $moveExtractedFileFrom $moveExtractedFileTo &&
 cd ~ &&
 echo "export PATH=/usr/local/lib/nodejs/node-v18.12.1-linux-x64/bin/:\$PATH" >> ~/.profile &&
-rm -rf ~/Downloads/node-v18.16.1-linux-x64.tar.xz &&
-rm -rf ~/Downloads/node-v18.16.1-linux-x64 &&
+rm -rf $tarFile &&
+rm -rf $extractedTarFile &&
 echo "\n\n_____________________________ Node V18 Installation success ___________________" &&
 echo "_______________________________ Please reboot your system _______________________" &&
 notify-send "Node v18 installation success" "Please reboot your device now" ||
